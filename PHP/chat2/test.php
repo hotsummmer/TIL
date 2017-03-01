0209c1e837f9517bb9c846453011cabdf393fe4c
hotsummmer <nerusaki@naver.com>
2017-03-01 16:48:40 +0900

    Create test.php

---
+++ PHP/chat2/test.php
@@ -0,0 +1,14 @@
+/* 기본 예제 2-1 */
+<?php
+echo "안녕하세요~~~";
+?>
+
+/* 기본 예제 2-2 */
+<html>
+  <head>
+    <title>PHP 뼈대 보기</title>
+  </head>
+  <body>
+    <?php echo "안녕하세요~~~"; ?>
+    </body>
+</html>w.
